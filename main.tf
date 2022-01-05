3808a6d75fd7a2c9e24516a299092ae356d7b892
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-05 23:40:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws"{
+  region = "us-east-2"
+}
+module "slave" {
+  source = "git::https://github.com/Gunamoni11/demo.git?ref=slave"
+}
+  module "master" {
+    source = "git::https://github.com/Gunamoni11/demo.git?ref=master"
+  }
+  
